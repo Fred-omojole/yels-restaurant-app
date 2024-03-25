b6de55ddfb8cf44fd2d9dbec43ff2fbfa8876801
Fred-Omojole Omoyele <omoyele.fred-omojole@stu.cu.edu.ng>
2024-03-25 13:08:58 +0100

    calendar setup

--- src/components/ReservationForm.tsx
+++ src/components/ReservationForm.tsx
@@ -25,10 +25,12 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useState } from "react";
 
 const ReservationForm = () => {
+  const [date, setDate] = useState();
   return (
-    <form>
+    <form className="">
       <div>
         <div className="grid grid-cols-1 xl:grid-cols-2 gap-[30px]">
           <div>
@@ -39,6 +41,33 @@ const ReservationForm = () => {
           <div>
             <label htmlFor="lastname">last name</label>
             <Input id="lastname" type="text" />
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-[30px]">
+          {/* calendar */}
+          <div>
+            <label> date </label>
+
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant={"input"}
+                  className={cn("w-full justify-start text-left font-normal")}
+                >
+                  <CalendarIcon className="mr-2 h-4 w-4" />
+                  {date ? format(date, "PPP") : <span>Pick a date</span>}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0">
+                <Calendar
+                  mode="single"
+                  selected={date}
+                  onSelect={setDate}
+                  initialFocus
+                />
+              </PopoverContent>
+            </Popover>
           </div>
         </div>
       </div>
