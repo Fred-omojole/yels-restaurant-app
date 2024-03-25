ef57311d3d699b53b4391fff95b46b5dccfcdb33
Fred-Omojole Omoyele <omoyele.fred-omojole@stu.cu.edu.ng>
2024-03-25 09:51:27 +0100

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import MenuBar from "@/components/MenuBar";
-import StyleGuide from "@/components/StyleGuide";
+// import StyleGuide from "@/components/StyleGuide";
 
 import Image from "next/image";
 
--- src/components/StyleGuide.tsx
+++ src/components/StyleGuide.tsx
@@ -1,119 +1,110 @@
-"use client"
-import * as React from "react"
-import { format } from "date-fns"
-import { Calendar as CalendarIcon } from "lucide-react"
+// "use client";
+// import * as React from "react";
+// import { format } from "date-fns";
+// import { Calendar as CalendarIcon } from "lucide-react";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
+// import { cn } from "@/lib/utils";
+// import { Button } from "@/components/ui/button";
+// import { Calendar } from "@/components/ui/calendar";
+// import {
+//   Popover,
+//   PopoverContent,
+//   PopoverTrigger,
+// } from "@/components/ui/popover";
 
-import Link from "next/link"
-// import { Button } from "./button"
-import { Label } from "./ui/label"
-import { Input } from "./ui/input"
-// import { Popover, PopoverTrigger } from "@radix-ui/react-popover"
+// import Link from "next/link";
+// // import { Button } from "./button"
+// import { Label } from "./ui/label";
+// import { Input } from "./ui/input";
+// // import { Popover, PopoverTrigger } from "@radix-ui/react-popover"
 
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+// import {
+//   Select,
+//   SelectContent,
+//   SelectGroup,
+//   SelectItem,
+//   SelectLabel,
+//   SelectTrigger,
+//   SelectValue,
+// } from "@/components/ui/select";
 
+// const StyleGuide = () => {
+//   const [date, setDate] = React.useState();
+//   return (
+//     <>
+//       {/* <div className="flex flex-col gap-y-4 p-24">
+//         <h1> Heading 1</h1>
+//         <h2>Heading 2</h2>
+//         <h3>Heading 3</h3>
 
-const StyleGuide = () => {
-  const [date, setDate] = React.useState()
-  return (
-  <>
-  <div className="flex flex-col gap-y-4 p-24">
-    <h1> Heading 1</h1>
-    <h2>Heading 2</h2>
-    <h3>Heading 3</h3>
+//         <p>
+//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sed?
+//           Nam ex soluta amet voluptatum corrupti eius omnis, labore iusto sunt
+//           eos provident distinctio eveniet veniam culpa ullam alias molestiae.
+//         </p>
+//       </div> */}
 
-    <p>
-      Lorem ipsum dolor sit amet 
-      consectetur adipisicing elit. Numquam, sed? 
-      Nam ex soluta amet voluptatum corrupti eius 
-      omnis, labore iusto sunt eos provident 
-      distinctio eveniet veniam culpa ullam 
-      alias molestiae.
-    </p>   
-  </div> 
+//       <div className="bg-black p-24 flex flex-col gap-y-4">
+//         <Link href="/">link</Link>
 
-  <div className="bg-black p-24 flex flex-col gap-y-4">
-  <Link href='/'>link</Link>
+//         <Button variant="default">Button 1</Button>
+//         <Button variant="orange">Button 2</Button>
+//         <Button variant="input">Button 3 </Button>
+//         <Button variant="orange" size="sm">
+//           Button 2
+//         </Button>
+//         {/* label & input */}
+//         <div>
+//           <Label htmlFor="firstname">First Name</Label>
+//           <Input type="firstname" id="firstname" />
+//         </div>
+//         <div>
+//           <Label htmlFor="lastname">Last Name</Label>
+//           <Input type="lastname" id="lastname" />
+//         </div>
+//         {/* calender */}
+//         <div>
+//           <Label> date </Label>
 
-  <Button variant="default">Button 1</Button>
-  <Button variant="orange">Button 2</Button>
-  <Button variant="input">Button 3 </Button>
-  <Button variant="orange" size="sm">
-    Button 2
-    </Button>
-{/* label & input */}
-<div>
-  <Label htmlFor="firstname">First Name</Label>
-  <Input type="firstname" id="firstname"/>
-</div>
-<div>
-  <Label  htmlFor="lastname">Last Name</Label>
-  <Input type="lastname" id="lastname"/>  
-</div>
-{/* calender */}
-<div>
-<Label> date </Label>
+//           <Popover>
+//             <PopoverTrigger asChild>
+//               <Button
+//                 variant={"input"}
+//                 className={cn("w-full justify-start text-left font-normal")}
+//               >
+//                 <CalendarIcon className="mr-2 h-4 w-4" />
+//                 {date ? format(date, "PPP") : <span>Pick a date</span>}
+//               </Button>
+//             </PopoverTrigger>
+//             <PopoverContent className="w-auto p-0">
+//               <Calendar
+//                 mode="single"
+//                 selected={date}
+//                 onSelect={setDate}
+//                 initialFocus
+//               />
+//             </PopoverContent>
+//           </Popover>
+//         </div>
+//         {/* select */}
+//         <Select>
+//           <SelectTrigger className="w-full">
+//             <SelectValue placeholder="Select a fruit" />
+//           </SelectTrigger>
+//           <SelectContent>
+//             <SelectGroup>
+//               <SelectLabel>Fruits</SelectLabel>
+//               <SelectItem value="apple">Apple</SelectItem>
+//               <SelectItem value="banana">Banana</SelectItem>
+//               <SelectItem value="blueberry">Blueberry</SelectItem>
+//               <SelectItem value="grapes">Grapes</SelectItem>
+//               <SelectItem value="pineapple">Pineapple</SelectItem>
+//             </SelectGroup>
+//           </SelectContent>
+//         </Select>
+//       </div>
+//     </>
+//   );
+// };
 
-<Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={"input"}
-          className={cn(
-            "w-full justify-start text-left font-normal"         
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          initialFocus
-        />
-      </PopoverContent>
-    </Popover>
-
-</div>
-{/* select */}
-<Select>
-      <SelectTrigger className="w-full">
-        <SelectValue placeholder="Select a fruit" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-</div>
-  
-  </>
-
-  );
-}
-
-export default StyleGuide
+// export default StyleGuide;
