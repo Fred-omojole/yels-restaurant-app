f216a733b95151a044914c741ee9d939442cead5
Fred-Omojole Omoyele <omoyele.fred-omojole@stu.cu.edu.ng>
2024-03-30 14:04:58 +0100

    fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -36,7 +36,7 @@ const Header = () => {
         <div className="flex items-center justify-between">
           {/* logo */}
           <Link href="/">
-            <Image src="/Logo.svg" width={75} height={30} alt="" />
+            <Image src="/logo.svg" width={75} height={30} alt="" />
           </Link>
           {/* navbar */}
           <Nav
