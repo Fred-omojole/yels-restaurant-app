15b93d49d38d47dca3b3142cff1b379484fcdf4a
Fred-Omojole Omoyele <omoyele.fred-omojole@stu.cu.edu.ng>
2024-03-30 13:49:55 +0100

    i don tire

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-// import Board from "@/components/Board";
 import About from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import MenuBar from "@/components/MenuBar";
 import Reservation from "@/components/Reservation";
-// import StyleGuide from "@/components/StyleGuide";
-import Map from "@/components/Map";
+
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -15,10 +14,7 @@ export default function Home() {
       <MenuBar />
       <Reservation />
       <About />
-      <Map />
-
-      {/* <StyleGuide /> */}
-      {/* <div className="h-[4000px]"></div> */}
+      <Footer />
     </main>
   );
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -52,48 +52,6 @@ const customIcon = new Icon({
 const Map = () => {
   const isMobile = useMediaQuery({
     query: "(max-width: 768px )",
-  });
-
-  return (
-    <>
-      <motion.section
-        variants={fadeIn("up", 0.2)}
-        initial="hidden"
-        whileInView={"show"}
-        viewport={{ once: false, amount: 0.2 }}
-        className="relative "
-        id="contact"
-      >
-        <MapContainer
-          center={[34.052235, -118.243683]}
-          zoom={isMobile ? 10 : 12}
-          className={`${isMobile ? "h-[300px]" : "h-[900px]"} z-10`}
-          zoomControl={false}
-        >
-          <TileLayer
-            attribution='&copy; <a href = "https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-          />
-          {/* markers */}
-          {markers.map((marker, index) => (
-            <Marker key={index} position={marker.position} icon={customIcon}>
-              <Popup>
-                <div className="flex gap-x-[30px]">
-                  <div className="flex-1">
-                    <h3>{marker.title}</h3>
-                    <p className="leading-snug">{marker.subtitle}</p>
-                  </div>
-                  <div className="flex-1">
-                    <Image
-                      src={marker.image}
-                      alt="image"
-                      width={130}
-                      height={160}
-                    />
-                  </div>
-                </div>
-              </Popup>
-            </Marker>
           ))}
         </MapContainer>
       </motion.section>
